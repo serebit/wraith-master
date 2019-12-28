d0193f95f28f690e8a9223c3aebe2951961f90b8
Campbell Jones <git@serebit.com>
2019-12-27 23:50:42 -0500

    Progress

--- src/linuxX64Main/kotlin/Main.kt
+++ src/linuxX64Main/kotlin/Main.kt
@@ -10,13 +10,53 @@ import libusb.*
 const val COOLER_MASTER_VENDOR_ID: UShort = 0x2516u
 const val WRAITH_PRISM_PRODUCT_ID: UShort = 0x51u
 
+const val ENDPOINT_IN: UByte = 0x83u
+const val ENDPOINT_OUT: UByte = 0x04u
+
 fun main() = memScoped {
     val init = libusb_init(null)
     check(init == 0) { "Failed to initialize libusb." }
 
+    print("Finding and opening Wraith Prism USB device... ")
     val device = findWraithPrismDevice()?.open() ?: error("Failed to find Wraith Prism USB device.")
+    println("Done.")
 
+    print("Resetting device... ")
+    libusb_reset_device(device.handle.ptr)
+    println("Done.")
+
+    print("Claiming interfaces... ")
     device.claimInterfaces()
+    println("Done.")
+
+    val outData = UByteArray(64)
+
+    ubyteArrayOf(0x41u, 0x80u).copyInto(outData)
+    device.transferOut(outData)
+    device.transferIn(64)
+
+    ubyteArrayOf(0x51u, 0x96u).copyInto(outData)
+    device.transferOut(outData)
+    device.transferIn(64)
+
+    ubyteArrayOf(0x51u, 0x28u, 0u, 0u, 0xe0u).copyInto(outData)
+    device.transferOut(outData)
+    device.transferIn(64)
+
+    outData.fill(0xffu)
+    ubyteArrayOf(0x51u, 0x2cu, 0x01u, 0u, 0x06u, 0xffu, 0u, 0x01u, 0xffu, 0xffu, 0xffu, 0xffu, 0xffu, 0u, 0u, 0u)
+        .copyInto(outData)
+    device.transferOut(outData)
+    device.transferIn(64)
+
+    device.transferOut(outData)
+    device.transferIn(64)
+
+    outData.fill(0u)
+    ubyteArrayOf(0x51u, 0xa0u, 0x01u, 0u, 0u, 0x03u, 0u, 0u, 0x05u, 0x06u).copyInto(outData)
+    UByteArray(14) { 0xfeu }.copyInto(outData, destinationOffset = 10)
+    device.transferOut(outData)
+    device.transferIn(64)
 
     device.close()
     libusb_exit(null)
@@ -41,7 +81,7 @@ fun loadUsbDevices(): List<libusb_device> = memScoped {
 fun MemScope.getUsbDeviceDescriptors(devices: List<CPointer<libusb_device>>) = devices.map {
     val descriptor = alloc<libusb_device_descriptor>()
     val err = libusb_get_device_descriptor(it, descriptor.ptr)
-    if (err != 0) println("Failed to load device with error code $err")
+    if (err != 0) println("Failed to load device descriptor with error code $err")
     descriptor
 }
 
@@ -77,4 +117,16 @@ class OpenedUsbDevice(device: libusb_device) {
     }
 
     fun close() = libusb_close(handle.ptr)
+}
+
+fun OpenedUsbDevice.transferIn(numBytes: Int): UByteArray = memScoped {
+    val bytes = UByteArray(numBytes)
+    val err = libusb_interrupt_transfer(handle.ptr, ENDPOINT_IN, bytes.toCValues().ptr, bytes.size, null, 1000u)
+    check(err == 0) { "Failed to transfer bytes to device OUT endpoint." }
+    bytes
+}
+
+fun OpenedUsbDevice.transferOut(bytes: UByteArray) = memScoped {
+    val err = libusb_interrupt_transfer(handle.ptr, ENDPOINT_OUT, bytes.toCValues().ptr, bytes.size, null, 1000u)
+    check(err == 0) { "Failed to transfer bytes to device OUT endpoint." }
 }
