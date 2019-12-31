120405039e34832d691879d1908d55dd8f65bd7d
Campbell Jones <git@serebit.com>
2019-12-30 21:25:31 -0500

    Add basic GTK application and bindings

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,25 @@ kotlin {
     linuxX64 {
         compilations["main"].cinterops {
             create("libusb") { includeDirs("/usr/include") }
+            create("gtk3") {
+                listOf("/opt/local/include", "/usr/include", "/usr/local/include").forEach {
+                    includeDirs(
+                        "$it/atk-1.0",
+                        "$it/gdk-pixbuf-2.0",
+                        "$it/cairo",
+                        "$it/harfbuzz",
+                        "$it/pango-1.0",
+                        "$it/gtk-3.0",
+                        "$it/glib-2.0"
+                    )
+                }
+
+                includeDirs(
+                    "/opt/local/lib/glib-2.0/include",
+                    "/usr/lib/x86_64-linux-gnu/glib-2.0/include",
+                    "/usr/local/lib/glib-2.0/include"
+                )
+            }
         }
         binaries.executable {
             entryPoint = "com.serebit.wraith.main"
