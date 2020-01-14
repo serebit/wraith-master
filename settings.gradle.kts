5eb6b5cd6b84964ec83dd529c3bde9850516266e
Campbell Jones <git@serebit.com>
2020-01-14 00:14:24 -0500

    Update Kotlin to 1.3.70-eap-42

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
 rootProject.name = "wraith-master"
 
 include(":core", ":gtk", ":cli")
+
+pluginManagement.repositories {
+    gradlePluginPortal()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
