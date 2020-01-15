6bd0b06612840cc240ab204e15ce3f33746fd9a0
Campbell Jones <git@serebit.com>
2020-01-15 17:36:56 -0500

    Revert to 1.3.61 due to issues with IntelliJ plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.3.70-eap-42" apply false
+    kotlin("multiplatform") version "1.3.61" apply false
     base // to add clean task, for cleaning up package output
 }
 
