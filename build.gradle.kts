7477aece6a691bd1a3759a6ff546a010245e963e
Campbell Jones <git@serebit.com>
2020-02-28 21:12:37 -0500

    Add distTar Gradle task to make my life easier

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,35 @@ plugins {
     base // to add clean task, for cleaning up package output
 }
 
-subprojects {
+allprojects {
     group = "com.serebit.wraith"
     version = "0.5.0-dev"
 
     repositories { 
         mavenCentral()
         maven("https://dl.bintray.com/kotlin/kotlin-eap") 
+    }
+}
+
+tasks.register("distTar") {
+    dependsOn(":cli:package")
+    dependsOn(":gtk:package")
+
+    doLast {
+        val packageDir = buildDir.resolve("package")
+        val tarballName = "wraith-master-$version.tar.xz"
+
+        temporaryDir.resolve("wraith-master").also {
+            packageDir.copyRecursively(it, true)
+            exec {
+                workingDir = temporaryDir
+                commandLine("tar", "cfJ", tarballName, it.name)
+            }
+        }
+
+        temporaryDir.resolve(tarballName).apply {
+            copyTo(buildDir.resolve("dist/$name"))
+            deleteOnExit()
+        }
     }
 }
