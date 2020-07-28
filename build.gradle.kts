a80fedd688b534b18b886ad19c456e1097d87ba3
Campbell Jones <git@serebit.com>
2020-07-28 16:04:53 -0400

    Add debian and tar dist tasks to gitlab-ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ tasks.register("distTar") {
 
             tempDir.resolve("wraith-master").setExecutable(true)
             tempDir.resolve("wraith-master-gtk").setExecutable(true)
-            exec { workingDir = temporaryDir; commandLine("tar", "cfJ", tarballName, tempDir.name) }
+            exec { workingDir = temporaryDir; commandLine("tar", "-cJf", tarballName, tempDir.name) }
         }
 
         temporaryDir.resolve(tarballName).copyTo(buildDir.resolve("dist/$tarballName"), true)
