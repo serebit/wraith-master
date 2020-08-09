94e53476ca9899b8da0076e7b179c939edbe448b
Campbell Jones <git@serebit.com>
2020-08-09 16:54:53 -0400

    Fix version detection, ready to land

--- core/src/linuxX64Main/kotlin/Version.kt
+++ core/src/linuxX64Main/kotlin/Version.kt
@@ -3,24 +3,22 @@ package com.serebit.wraith.core
 import kotlinx.cinterop.*
 import platform.posix.*
 
-private val executablePath = memScoped {
+@OptIn(ExperimentalUnsignedTypes::class)
+val programVersion = memScoped {
     val pathBuffer = allocArray<ByteVar>(PATH_MAX)
     val nchar = readlink("/proc/self/exe", pathBuffer, PATH_MAX)
-
     check(nchar >= 0) { "Failed to get path to running executable" }
-    pathBuffer.toKString().replaceAfterLast('/', "").replaceAfterLast('/', "")
-}
 
-@OptIn(ExperimentalUnsignedTypes::class)
-val programVersion = "${executablePath}share/wraith-master/version.txt"
-    .takeIf { access(it, R_OK) != -1 }
-    ?.let { path ->
-        val file = fopen(path, "r")!!
-        memScoped {
+    val installRoot = pathBuffer.toKString().replaceAfterLast('/', "").removeSuffix("/bin/")
+
+    "${installRoot}/share/wraith-master/version.txt"
+        .takeIf { access(it, R_OK) != -1 }
+        ?.let { path ->
+            val file = fopen(path, "r")!!
             val fileLength = alloc<stat>().apply { fstat(fileno(file), ptr) }.st_size
             allocArray<ByteVar>(fileLength + 1).apply {
                 fread(this, fileLength.toULong(), 1u, file)
                 fclose(file)
             }.toKString().trim()
         }
-    }
+}
