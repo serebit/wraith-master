aa7f6c64edacd8b6470be6f7e4306c777d119f75
Campbell Jones <git@serebit.com>
2021-03-30 18:37:27 -0400

    Update Kotlin to 1.5.0-M2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.31" apply false
-    id("com.github.ben-manes.versions") version "0.36.0"
+    kotlin("multiplatform") version "1.5.0-M2" apply false
+    id("com.github.ben-manes.versions") version "0.38.0"
     base // to add clean task, for cleaning up package output
 }
 
