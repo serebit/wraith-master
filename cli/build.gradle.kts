f47db06bbb2cd8146c2b5c1892295e9098643095
Campbell Jones <git@serebit.com>
2021-05-16 14:58:53 -0400

    remove package/install tasks from Gradle scripts

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -12,63 +12,9 @@ kotlin.linuxX64 {
 
     binaries.executable {
         entryPoint = "com.serebit.wraith.cli.main"
-        linkerOpts("--as-needed", "-z", "now", "-z", "relro")
+        linkerOpts("--as-needed")
         freeCompilerArgs = freeCompilerArgs + listOf("-Xoverride-konan-properties=linkerGccFlags=-lgcc -lgcc_eh -lc")
     }
-}
-
-val `package` by tasks.registering {
-    dependsOn(":core:package", "linkReleaseExecutableLinuxX64", "linuxX64ProcessResources")
-
-    doLast {
-        val packageDir = rootProject.buildDir.resolve("package")
-
-        val shouldStrip = properties["strip"].let { it is String && (it.isEmpty() || it == "true") }
-        val disableManPages = properties["disable-man-pages"].let { it is String && (it.isEmpty() || it == "true") }
-
-        buildDir.resolve("bin/linuxX64/releaseExecutable/cli.kexe")
-            .copyTo(packageDir.resolve("wraith-master"), overwrite = true)
-            .also {
-                if (shouldStrip) exec {
-                    commandLine("strip", it.absolutePath)
-                }
-                it.setExecutable(true, false)
-            }
-
-        if (!disableManPages) {
-            val scdPath = projectDir.resolve("resources/wraith-master.1.scd").path
-            val manPath = packageDir.resolve("resources/wraith-master.1").path
-            exec { commandLine("sh", "-c", "scdoc < $scdPath > $manPath") }
-        }
-    }
-}
-
-val prepareInstall by tasks.registering {
-    dependsOn(`package`)
-
-    doLast {
-        val packageDir = rootProject.buildDir.resolve("package")
-        val resourcesDir = packageDir.resolve("resources")
-        val destDir = buildDir.resolve("preparedInstall")
-        val disableManPages = properties["disable-man-pages"].let { it is String && (it.isEmpty() || it == "true") }
-
-        packageDir.resolve("wraith-master")
-            .copyTo(destDir.resolve("bin/wraith-master"), overwrite = true)
-            .also { it.setExecutable(true, false) }
-
-        if (!disableManPages) {
-            resourcesDir.resolve("wraith-master.1")
-                .takeIf { it.exists() }
-                ?.copyTo(destDir.resolve("share/man/man1/wraith-master.1"), overwrite = true)
-        }
-    }
-}
-
-tasks.register<Copy>("install") {
-    dependsOn(":core:install", prepareInstall)
-
-    from(buildDir.resolve("preparedInstall"))
-    destinationDir = file(properties["prefix"] ?: "/usr/local")
 }
 
 tasks.register("valgrind") {
